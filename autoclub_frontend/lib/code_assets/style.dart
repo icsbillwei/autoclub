41ff548a6503474db6826bb7e447850c1854b250
icsbillwei <shuheng.wei@outlook.com>
2024-02-03 17:07:36 -0500

    browser

--- autoclub_frontend/lib/code_assets/style.dart
+++ autoclub_frontend/lib/code_assets/style.dart
@@ -4,10 +4,25 @@ import 'package:flutter/material.dart';
 const dark = Color.fromRGBO(62, 62, 62, 1);
 const whiteTranslucent = Color.fromRGBO(255, 255, 255, 0.8);
 const blue = Color.fromRGBO(0, 165, 217, 1);
+const bgWhite = Color.fromRGBO(248, 248, 248, 1);
+const darkWhite = Color.fromRGBO(225, 225, 225, 1);
 
 ThemeData lightTheme = ThemeData(
-  textTheme: const TextTheme(
 
+  colorScheme: const ColorScheme(
+      brightness: Brightness.light,
+      primary: blue, // white
+      onPrimary: whiteTranslucent,
+      secondary: dark,
+      onSecondary: darkWhite,
+      error: Colors.red,
+      onError: Colors.red,
+      background: bgWhite,
+      onBackground: bgWhite,
+      surface: bgWhite,
+      onSurface: bgWhite),
+
+  textTheme: const TextTheme(
     // Main menu => location description => title
     displayMedium: TextStyle(
       fontWeight: FontWeight.w500,
--- autoclub_frontend/lib/pages/home.dart
+++ autoclub_frontend/lib/pages/home.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:autoclub_frontend/code_assets/texts.dart';
 import 'package:autoclub_frontend/main.dart';
 import 'package:autoclub_frontend/code_assets/style.dart';
@@ -48,25 +50,25 @@ class _MyHomePageState extends State<MyHomePage> {
     return Stack(children: <Widget>[
       SizedBox(
           child: GestureDetector(
-        child: InteractiveViewer(
-            // scaleFactor: 0.3,
-            minScale: 0.7,
-            maxScale: 0.7,
-            interactionEndFrictionCoefficient: 0.02,
-            constrained: false,
-            transformationController: viewTransformationController,
-            child: Stack(children: <Widget>[
-              // Background image
-              Image.asset("images/game-bg.png"),
+            child: InteractiveViewer(
+                // scaleFactor: 0.3,
+                minScale: 0.7,
+                maxScale: 0.7,
+                interactionEndFrictionCoefficient: 0.02,
+                constrained: false,
+                transformationController: viewTransformationController,
+                child: Stack(children: <Widget>[
+                  // Background image
+                  Image.asset("images/game-bg.png"),
 
-              // Items on map
-              mapItem(1600, 700, "images/downtown.svg", Location.downtown),
-              mapItem(2150, 950, "images/home.svg", Location.home),
-              mapItem(2000, 450, "images/hotel.svg", Location.hotel),
-              mapItem(2350, 600, "images/showroom.svg", Location.showroom),
-              mapItem(2760, 1200, "images/tuning.svg", Location.tuning),
-              mapItem(950, 600, "images/wharf.svg", Location.wharf),
-            ])),
+                  // Items on map
+                  mapItem(1600, 700, "images/downtown.svg", Location.downtown),
+                  mapItem(2150, 950, "images/home.svg", Location.home),
+                  mapItem(2000, 450, "images/hotel.svg", Location.hotel),
+                  mapItem(2350, 600, "images/showroom.svg", Location.showroom),
+                  mapItem(2760, 1200, "images/tuning.svg", Location.tuning),
+                  mapItem(950, 600, "images/wharf.svg", Location.wharf),
+                ])),
       )),
 
       // Location Entry Prompt
@@ -75,8 +77,109 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Container(
               margin: const EdgeInsets.only(bottom: 40),
               child: locationEntrance())),
+      
+      // Browser 
+      Align(
+        alignment: Alignment.bottomRight,
+        child: Padding(
+          padding: const EdgeInsets.all(30),
+          child: FloatingActionButton.extended(
+            elevation: 5,
+            onPressed: () {
+              showDialog(context: context, builder:  (BuildContext context) {
+                return browser();
+              });
+            },
+            label: Text(
+              "Browser",
+              style: Theme.of(context).textTheme.labelMedium,
+            ),
+            icon: const Icon(Icons.language_rounded),
+          ),
+        ),
+      )
+
+
     ]);
   }
+
+
+  Widget browser() {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return SimpleShadow(
+      color: Colors.black,
+      sigma: 10,
+      child: Dialog(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+        surfaceTintColor: Theme.of(context).colorScheme.background,
+        child: Container(
+          width: width * 0.8,
+          height: height * 0.8,
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+            child: Column(
+              children: [
+
+                // Navbar
+                Container(
+                  height: 50,
+                  child: Row(
+                    children: [
+                      SvgPicture.asset("images/Corone-logo.svg", width: 80,),
+                      SizedBox(width: 20,),
+                      Expanded(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).colorScheme.onSecondary,
+                            borderRadius: BorderRadius.circular(20)
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 20,),
+                      SizedBox(
+                        height: 50,
+                        child: FittedBox(
+                          child: IconButton(
+                              onPressed: (){
+                          
+                              },
+                              icon: Icon(
+                                Icons.home, 
+                                color: Theme.of(context).colorScheme.secondary,
+                              )
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 50,
+                        child: FittedBox(
+                          child: IconButton(
+                              onPressed: (){
+                                Navigator.pop(context);
+                              },
+                              icon: Icon(
+                                Icons.close_rounded, 
+                                color: Theme.of(context).colorScheme.secondary,
+                              )
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+
+                // Content
+
+
+              ],
+            ),
+          )
+        ),
+      ),
+    );
+  }
+
 
   Widget mapItem(double posX, double posY, String img, Location toggle) {
     bool thisToggled = location == toggle;
@@ -120,7 +223,7 @@ class _MyHomePageState extends State<MyHomePage> {
               height: 130,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(30),
-                color: whiteTranslucent,
+                color: lightTheme.colorScheme.onPrimary,
               ),
               child: Padding(
                 padding:
@@ -130,7 +233,10 @@ class _MyHomePageState extends State<MyHomePage> {
                     Align(
                       alignment: Alignment.topRight,
                       child: IconButton(
-                        icon: const Icon(Icons.close_rounded),
+                        icon: Icon(
+                          Icons.close_rounded,
+                          color: Theme.of(context).colorScheme.secondary
+                        ),
                         onPressed: () {
                           setState(() {
                             location = Location.undefined;
